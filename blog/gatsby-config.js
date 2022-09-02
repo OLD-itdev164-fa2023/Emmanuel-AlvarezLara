0c4a0a5c9c035fcb9b2fa5c377151d51cf1a8809
Emmanuel Alvarez Lara <dc_luk@hotmail.com>
2022-09-01 20:54:30 -0500

    update

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     contact: {
       name:'Emmanuel',
       company:'Blong Inc',
-      address:'160 W crawford',
+      address:'160 W Crawford Ave Milwaukee Wi.',
 
     }
   },
